26a5db0faf95c96d4d7bdcfc91728a3ff806ac76
TaYaKi71751 <shantian.tw@gmail.com>
2023-01-02 13:50:28 +0900

    Add more event listeners

--- hide.js
+++ hide.js
@@ -1,6 +1,6 @@
 (async () => {
 	const hideSomeAnalyticsViewElement = async () => {
-		(async() => {document.querySelectorAll(
+		(async () => {document.querySelectorAll(
 			'[href*=analytics][aria-label]'
 		).forEach(async (e)=>{
 			e.parentNode.remove();
@@ -11,24 +11,11 @@
 			e.parentNode.remove();
 		})})();
 	};
-	(async () => {document.addEventListener("scroll",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("scroll",hideSomeAnalyticsViewElement)})();
- (async () => {document.addEventListener("click",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("animation",hideSomeAnalyticsViewElement)})();
- (async () => {document.addEventListener("change",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("focus",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("waiting",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("load",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("loadend",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("loadstart",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("progress",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("load",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("readystatechange",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("DOMContentLoaded",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("loadeddata",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("play",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("canplay",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("canplaythrough",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("timeupdate",hideSomeAnalyticsViewElement)})();
-	(async () => {document.addEventListener("durationchange",hideSomeAnalyticsViewElement)})();
+	const events = Object.keys(document.__proto__.__proto__).reduce((arr, event)=> { // https://stackoverflow.com/questions/39963850/how-can-i-add-an-event-listener-for-all-events-in-javascript-without-listing-the
+		if(event.startsWith('on')) return [...arr, event.substr(2)];
+		return arr;
+	}, []);
+	events.forEach(async (event) => {
+		document.addEventListener(event,hideSomeAnalyticsViewElement);
+	});
 })();
